26998e6b14bed19948082d6a2065ee68df2dfa2e
MihaiChirculete <mihai.chirculete98@gmail.com>
2022-10-01 12:31:27 +0300

    Added host and port configs

--- utils/config_provider.py
+++ utils/config_provider.py
@@ -6,6 +6,8 @@ class ConfigProvider:
         config = ConfigParser()
         config.read("config.properties")
 
+        self.deck_host = str(config.get("SSH-Section", "deck.host"))
+        self.deck_port = int(config.get("SSH-Section", "deck.port"))
         self.deck_user = str(config.get("SSH-Section", "deck.user"))
         self.deck_pass = str(config.get("SSH-Section", "deck.password"))
         self.deck_reconnect_delay = int(config.get("SSH-Section", "deck.reconnect_delay"))
