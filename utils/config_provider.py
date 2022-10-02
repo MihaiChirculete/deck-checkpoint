daf8c00fc4308b9f32584db31dfe157ec8dbd49d
MihaiChirculete <mihai.chirculete98@gmail.com>
2022-10-02 09:39:44 +0300

    Removed local saves dir logic as saves will be directly pulled to their appropriate locations based on defined schemas

--- utils/config_provider.py
+++ utils/config_provider.py
@@ -2,6 +2,7 @@ import os
 from configparser import ConfigParser
 import platform
 
+
 class ConfigProvider:
     def __init__(self):
         config = ConfigParser()
@@ -9,19 +10,10 @@ class ConfigProvider:
 
         self.local_platform = str(platform.system()).upper()
 
-        self.local_saves_dir = str(config.get("AppSettings", "checkpoint.saves_dir"))
+        self.auto_pull_saves = bool(config.get("AppSettings", "checkpoint.automatically_pull_all_saves"))
 
         self.deck_host = str(config.get("SSH-Section", "deck.host"))
         self.deck_port = int(config.get("SSH-Section", "deck.port"))
         self.deck_user = str(config.get("SSH-Section", "deck.user"))
         self.deck_pass = str(config.get("SSH-Section", "deck.password"))
         self.deck_reconnect_delay = int(config.get("SSH-Section", "deck.reconnect_delay"))
-
-        # Call the setup in order to make sure all required dirs are created and up to date with the config
-        self.setup()
-
-    def setup(self):
-        # Check if the local saves dir exists and create it otherwise
-        if not os.path.isdir(self.local_saves_dir):
-            print(f"Saves dir doesn't exist, creating it at: {self.local_saves_dir}")
-            os.makedirs(self.local_saves_dir)
