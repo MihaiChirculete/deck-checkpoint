316cb58476f8e91fca40a2845c6645f28ab0a81e
MihaiChirculete <mihai.chirculete98@gmail.com>
2022-10-02 12:47:09 +0300

    Logic for pulling remote saves done

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
+import os
 
+from utils.providers import ConfigProvider, SchemaProvider
+from utils.save_tool import SaveTool
+import paramiko
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    cp = ConfigProvider()
+    cp.project_root = os.path.dirname(os.path.abspath(__file__))
+
+    sp = SchemaProvider(config_provider=cp)
+
+    print(cp.local_platform)
+
+    if "LINUX" != cp.local_platform:
+        print("This tool is only supported on linux for now")
+        exit()
+
+    # Open a transport
+    print(f"Attempting to connect to your steam deck at: {cp.deck_user}@{cp.deck_host}")
+    transport = paramiko.Transport((cp.deck_host, cp.deck_port))
+
+    # Auth
+    print(f"Attempting to authenticate on Steam Deck as '{cp.deck_user}'")
+    transport.connect(None, cp.deck_user, cp.deck_pass)
+
+    # Instantiate a SaveTool object and pass it a SFTP client and the config provider
+    st = SaveTool(sftp_client=paramiko.SFTPClient.from_transport(transport), config_provider=cp, schema_provider=sp)
+    st.get_installed_app_ids()
+    st.pull_remote_saves()
+
+    # Close
+    if st.client: st.teardown()
+    if transport: transport.close()
